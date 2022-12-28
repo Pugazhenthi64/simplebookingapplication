f7b794c774488712bf3f61b314c7502a87281938
Pugazhenthi64 <spugazhenthi64@yahoo.com>
2022-12-28 21:02:47 +0530

    simplebookingapplication

---
+++ src/main/java/com/te/simple_booking_system/entity/App.java
@@ -0,0 +1,5 @@
+package com.te.simple_booking_system.entity;
+
+public class App {
+
+}
---
+++ src/main/java/com/te/simple_booking_system/entity/DefaultUsers.java
@@ -0,0 +1,19 @@
+package com.te.simple_booking_system.entity;
+
+@da
+public class DefaultUsers {
+
+	private Short id;
+	private String email;
+	private String password;
+	private String salt;
+	private Integer groupId;
+	private String ipAddress;
+	private Boolean active;
+	private String activationCode;
+	private Integer createdOn;
+	private String userName;
+	private String forgottenPasswordCode;
+	private String rememberCode;
+	
+}
